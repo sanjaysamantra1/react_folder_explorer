dad27ac8eec5bbf70ee60ceb546fcc5d1fd5c460
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-02-08 21:35:58 +0530

    folder

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -4,28 +4,25 @@ export default function Folder({ folderInfo }) {
   const [expand, setExpand] = useState(false);
 
   if (folderInfo.isFolder) { // if folder
-    return (
-      <div>
-        <span onClick={() => setExpand(!expand)}>
-          📁
-          {folderInfo.name}
-          <br />
-        </span>
-        <div
-          style={{ display: expand ? "block" : "none", paddingLeft: "20px" }}
-        >
-          {folderInfo.items.map((subfolderInfo, ind) => (
-            <Folder folderInfo={subfolderInfo} key={ind} />
-          ))}
-        </div>
-      </div>
-    );
-  } else { // if file
-    return (
-      <span>
-        🏢
-        {folderInfo.name} <br />
+    return <div>
+      <span onClick={() => setExpand(!expand)} style={{ cursor: 'pointer' }}>
+        📁
+        {folderInfo.name}
+        <br />
       </span>
-    );
+      <div
+        style={{ display: expand ? "block" : "none", paddingLeft: "20px" }}
+      >
+        {folderInfo.items.map((subfolderInfo, ind) => (
+          <Folder folderInfo={subfolderInfo} key={ind} />
+        ))}
+      </div>
+    </div>
+  } else { // if file
+    return <span>
+      🏢
+      {folderInfo.name} <br />
+    </span>
+
   }
 }
